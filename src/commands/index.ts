f9db3f372fb46b2d321dccea40b6e8e12a3552e0
ChocoGreen <33989421+choco-green@users.noreply.github.com>
2021-10-02 21:27:18 +0100

    Github cache clearing

--- src/commands/index.ts
+++
@@ -1,7 +0,0 @@
-export { play } from "./play";
-export { skip } from "./skip";
-export { stop } from "./stop";
-export { queue } from "./queue";
-export { nowPlaying } from "./nowPlaying";
-export { shuffle } from "./shuffle";
-export { help } from "./help";
